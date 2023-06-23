8ff3f59ec48bcecd4601635513f6040ded67aff7
trungmc <trungmc@yahoo.com>
2023-06-23 07:56:59 +0700

    Format a little bit

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import requests
 import pandas as pd
 from chat_downloader import ChatDownloader
 from pytube import Playlist, YouTube
+import pickle
+import json
 
 
 def get_chat_replay(video_id):
-    chat = ChatDownloader().get_chat("https://www.youtube.com/watch?v=" + video_id,
-                                     )
-
+    chat = ChatDownloader().get_chat("https://www.youtube.com/watch?v=" + video_id)
     return chat
 
 
@@ -19,7 +19,7 @@ def get_live_videos(channel_id, api_key):
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&eventType=live&type=video&key={api_key}"
     response = requests.get(url)
     data = response.json()
-    #print(data)
+    # print(data)
     return data['items']
 
 
@@ -29,7 +29,7 @@ def get_past_streams(channel_id, api_key):
     items = []
     while True:
         response = requests.get(url + (f"&pageToken={next_page_token}" if next_page_token else ""))
-        #print(response.content)
+        # print(response.content)
         data = response.json()
         items.extend(data['items'])
         next_page_token = data.get('nextPageToken')
@@ -39,22 +39,27 @@ def get_past_streams(channel_id, api_key):
     return items
 
 
-def export_chat_to_excel(chat_content):
-    excel_lines=[]
+def chat_content_to_df(chat_content):
+    excel_lines = []
     for message in chat_content:
-
+        image_url = next(image['url'] for image in message['author']['images'] if image['id'] == '32x32')
         if '?' in message['message']:
-            excel_lines.append([message['author']['name'],message['message']])
-
+            excel_lines.append([message['author']['name'], message['message'], '1', message['message_type'],
+                                message['time_in_seconds'], message['author']['id'], image_url])
+        else:
+            excel_lines.append([message['author']['name'], message['message'], '0', message['message_type'],
+                                message['time_in_seconds'], message['author']['id'], image_url])
     df = pd.DataFrame(excel_lines,
-                      columns=['Author', 'Message'])
+                      columns=['Author', 'Message', 'IsQuestion', 'MessageType', 'TimeinSeconds', 'AuthorID', 'Avatar'])
+    return df
 
-    # df.to_excel(file_name, index=False)
+
+def export_chat_to_excel(df):
     excel_file = BytesIO()
     df.to_excel(excel_file, index=False)
     excel_file.seek(0)
-
     return excel_file
+
 
 # {'time_in_seconds': 4996.176, 'action_type': 'add_chat_item', 'message': 'không sao đâu phan sơn mọi người hiểu mà', 'message_id': 'ChwKGkNNaTlxZWpqMF84
 # Q0ZiekN3Z1FkdXo0RHdR', 'timestamp': 1687330465952199, 'time_text': '1:23:16', 'author': {'name': 'Nhuan Nguyen', 'images': [{'url': 'https://yt4.ggpht.c
@@ -77,7 +82,7 @@ def main():
 
     # # Create a sidebar dropdown box to select a video
     selected_video_title = st.sidebar.selectbox("Select a Video. Most recent at top", options=all_videos)
-
+    question_only = st.sidebar.checkbox('Question Only', value="True")
     css = """
     <style>
       .avatar {
@@ -97,17 +102,17 @@ def main():
 
         selected_video_id = selected_video_title
         metadata = YouTube('https://www.youtube.com/watch?v=' + selected_video_id)
-        #print(metadata.title)
-        # Retrieve chat content for the selected video
+
         url = f'https://www.youtube.com/watch?v={selected_video_id}'
         try:
             chat_replay = get_chat_replay(selected_video_id)
+            df = chat_content_to_df(chat_replay)
 
             col1, col2, col3 = st.columns(3)
             if col1.button("Export Chat to Excel"):
                 # Export chat content to Excel
 
-                excel_file = export_chat_to_excel(chat_replay)
+                excel_file = export_chat_to_excel(df)
                 file_name = f'{selected_video_id}.xlsx'
                 col2.download_button(
                     label="Download data as Excel",
@@ -120,11 +125,15 @@ def main():
             # Display the chat content
             st.subheader(metadata.title)
 
-            for message in chat_replay:
-                #print(message)
-                if '?' in message['message']:
-                    image_url = next(image['url'] for image in message['author']['images'] if image['id'] == '32x32')
-                    st.markdown(f"<div><div class='avatar'><img src='{image_url}' alt='{message['author']['name']}'></img></div> <b>{message['author']['name']}</b> : {message['message']}</div>",unsafe_allow_html=True                                )
+            if question_only:
+                display_df = df[df["IsQuestion"] == '1']
+            else:
+                display_df = df
+            print(display_df)
+            for index, l in display_df.iterrows():
+                st.markdown(
+                    f"<div><div class='avatar'><img src='{l.Avatar}' alt='{l.Author}'></img></div> <b>{l.Author}</b> : {l.Message}</div>",
+                    unsafe_allow_html=True)
 
         except chat_downloader.errors.NoChatReplay:
             st.error('No Chat Replay for this Video')
